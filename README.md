cc6518b44ccd2529765777abe8001e50980d63a6
vnik <vnik@mbp13.local>
2015-04-11 01:50:26 +1000

    merged public and private queues

--- README.md
+++ README.md
@@ -7,28 +7,21 @@ Currently, the following algorithms are supported:
 
 * MD5
 * SHA1
-* LM
 * NTLM
 * WPA / WPA2
-* Cisco type 7
-* Windows Group Policy Preferences (GPP) cpassword
 * DESCRYPT / DES(Unix) 
 * MD5CRYPT / MD5(Unix) 
 
 WPA/WPA2
 --------
 
-To submit your your handshake to the private queue:
+To submit your your handshake:
 
 `$ wpaclean out.cap captured.cap`
 
 `$ aircrack-ng out.cap -J /tmp/tosubmit`
 
 `$ ./crackqcli.py -t wpa /tmp/tosubmit.hccap`
-
-The private queue submission is similar except for the `-q privq` switch:
-
-`$ ./crackqcli.py -q privq -t wpa /tmp/tosubmit.hccap`
 
 MD5/SHA1/NTLM
 -------------
@@ -37,49 +30,21 @@ MD5/SHA1/NTLM
 
 `$ ./crackqcli.py -t ntlm ab54d7ae46a72b05388ec24611c96a2d`
 
-To submit to the private queue, use the `-q privq` switch:
-
-`$ ./crackqcli.py -q privq -t md5 06aa3b7d55df43e7d7fa4aef94811e4a`
-
-`$ ./crackqcli.py -q privq -t ntlm ab54d7ae46a72b05388ec24611c96a2d`
-
-SHA1 hashes (40 hex chars) are supported by the private queue only:
+SHA1 hashes are 40 hex characters:
 
 `$ ./crackqcli.py -q privq -t sha1 35029a2e592be14756b3bd91fbf873d9e2885713`
-
-LM
---
-
-The LM hash is submitted as 32 hex-char value, i.e., two halves of the password:
- 
-`$ ./crackqcli.py -t lm f6dc2e4c788de157ff17365faf1ffe89`
-
-For more information regarding LM hashes refer to the [Crackq FAQ](http://hashcrack.org/crackq_faq)
 
 DESCRYPT/DES(Unix)
 ------------------
 
-DES-based Unix crypt algorithm is supported by the private queue only:
-
-`$ ./crackqcli.py -q privq -t descrypt ffTEQtUBN6Glk`
+`$ ./crackqcli.py -t descrypt ffTEQtUBN6Glk`
 
 MD5CRYPT/MD5(Unix)
 ------------------
 
-MD5-based Unix crypt algorithm is supported by the private queue only:
+`$ ./crackqcli.py -t md5crypt '$1$abcdefgh$WSwV3CmjYt3iE5AlESn9Z.'`
 
-`$ ./crackqcli.py -q privq -t md5crypt '$1$abcdefgh$WSwV3CmjYt3iE5AlESn9Z.'`
+IPSec IKE PSK
+-------------
 
-Cisco type 7/Windows Group Policy Preferences (GPP)
------------------------------------------------------
-
-These are encodings which are reversible. There is no processing time
-involved.
-
-To submit your Cisco type 7 hash:
-
-`$ ./crackqcli.py -t cisco_type7 your_cisco_type7_hash`
-
-To submit your Windows GPP (Group Policy Preferences) `cpassword` hash (found in Groups.xml):
-
-`$ ./crackqcli.py -t gpp your_cpassword_hash`
+Refer to (https://hashcrack.org/crackq/page?n=ike) for details.
--- crackqcli.py
+++ crackqcli.py
@@ -19,30 +19,24 @@ ENDPOINTS = {
                 'client_ver' : '/crackq/v0.1/client_ver'
             }
 API_KEY = None
-MYVER = '0.19b'
-PRIVQ_HASH_TYPES = ['wpa', 'descrypt', 'md5crypt', 'md5', 'ntlm', 'sha1', 'ike_md5']
-PUBQ_HASH_TYPES  = ['lm', 'ntlm', 'md5', 'wpa', 'gpp', 'cisco_type7']
+MYVER = '0.2b'
+HASH_TYPES = ['wpa', 'descrypt', 'md5crypt', 'md5', 'ntlm', 'sha1', 'ike_md5']
 
 def banner():
     sys.stdout.write('hashcrack.org crackq client %s\n\n' % MYVER)
 
 def usage(argv0):
-    sys.stdout.write('%s [-q privq|pubq] [-t|--type hash_type] [hash|file_path]\n' % argv0)
-    sys.stdout.write('-t --type        see supported formats below\n')
-    sys.stdout.write('-q               queue type: pubq or privq\n') 
+    sys.stdout.write('%s [-t|--type hash_type] [hash|file_path]\n' % argv0)
+    sys.stdout.write('-t --type        see supported hash types below\n')
     sys.stdout.write('-h --help        help\n\n')
-    sys.stdout.write('Supported formats:\n')
+    sys.stdout.write('Supported hash types:\n')
     sys.stdout.write('md5              Unsalted MD5 hashes\n')
     sys.stdout.write('ntlm             Windows NTLM hashes\n')
-    sys.stdout.write('lm               Windows LM hashes\n')
-    sys.stdout.write('gpp              Windows Group Policy Preferences (GPP)\n')
-    sys.stdout.write('sha1             Unsalted SHA1 hashes (*)\n')
-    sys.stdout.write('cisco_type7      Cisco type 7 hashes\n')
+    sys.stdout.write('sha1             Unsalted SHA1 hashes\n')
     sys.stdout.write('wpa              WPA/WPA2 handshakes\n')
-    sys.stdout.write('md5crypt         MD5CRYPT / FreeBSD MD5 / Cisco IOS MD5 / MD5(Unix) (*)\n')
-    sys.stdout.write('descrypt         DESCRYPT / DES(Unix) (*)\n')
-    sys.stdout.write('ike_md5          VPN IPSec IKE (MD5) preshared keys (*)\n\n')
-    sys.stdout.write('Hash types marked by (*) are supported by the privq only.\n')
+    sys.stdout.write('md5crypt         MD5CRYPT / FreeBSD MD5 / Cisco IOS MD5 / MD5(Unix)\n')
+    sys.stdout.write('descrypt         DESCRYPT / DES(Unix)\n')
+    sys.stdout.write('ike_md5          VPN IPSec IKE (MD5) preshared keys\n')
 
 def validate_hash(_hash, _hash_type):
     if _hash_type == 'descrypt':
@@ -50,9 +44,6 @@ def validate_hash(_hash, _hash_type):
            return False
     elif _hash_type == 'md5crypt':
        if re.match('^\$1\$[\./0-9A-Za-z]{0,8}\$[\./0-9A-Za-z]{22,22}$', _hash) is None:
-           return False
-    elif _hash_type == 'cisco_type7':
-       if re.match('^[0-9][0-9][0-9A-Fa-f]+$', _hash) is None:
            return False
     elif _hash_type == 'sha1':
         if len(_hash) != 40:
@@ -63,13 +54,6 @@ def validate_hash(_hash, _hash_type):
         except ValueError:
             sys.stdout.write('[-] ERROR: The hash is not in hex\n')
             return False
-    elif _hash_type == 'gpp':
-       # pad it if needed
-       base64str_pad=_hash + (4 - len(_hash)%4) * '='
-       try:
-           cpassword = base64.b64decode(base64str_pad)
-       except TypeError:
-           return False
     else:
         if len(_hash) != 32:
             sys.stdout.write('[-] ERROR: Invalid hash\n')
@@ -108,7 +92,6 @@ def load_config():
     else:
         sys.stdout.write('[-] ERROR: Unsupported OS\n')
         sys.exit(-1)
-
     try:
         conf = open(CONFIG_PATH, 'r')
         for l in conf.readlines():
@@ -123,8 +106,6 @@ def load_config():
 
 if __name__ == '__main__':
     _type = None
-    qtype = 'pubq'
-
     banner()
 
     try:
@@ -143,15 +124,10 @@ if __name__ == '__main__':
            sys.exit(0)
        if o in ('-t', '--type'):
            _type = a
-       if o == '-q':
-	   if a not in ('pubq', 'privq'):
-	       sys.stdout.write('[-] ERROR: INVALID QUEUE TYPE\n')
-	       sys.exit(-1)
-           qtype = a
 
     # check for updates
     if urlopen(SERVER + ENDPOINTS['client_ver']).read() != MYVER:
-        sys.stdout.write('[-] WARNING: New client version is available. Please update.')
+        sys.stdout.write('[-] WARNING: NEW CLIENT VERSION IS AVAILABLE. PLEASE UPDATE.')
         sys.exit(-1)
 
     if len(args) != 1:
@@ -160,16 +136,8 @@ if __name__ == '__main__':
 
     _content = args[0]
      
-    if not _type or (_type not in PRIVQ_HASH_TYPES and _type not in PUBQ_HASH_TYPES):
+    if not _type or _type not in HASH_TYPES:
         sys.stdout.write('[-] ERROR: INVALID HASH TYPE\n')
-        sys.exit(-1)
-
-    if qtype == 'privq' and _type not in PRIVQ_HASH_TYPES:
-        sys.stdout.write('[-] ERROR: NOT SUPPORTED BY PRIVQ\n')
-        sys.exit(-1)
-
-    if qtype == 'pubq' and _type not in PUBQ_HASH_TYPES:
-        sys.stdout.write('[-] ERROR: NOT SUPPORTED BY PUBQ\n')
         sys.exit(-1)
 
     if (_type != 'wpa' and _type != 'ike_md5') and not validate_hash(_content, _type):
@@ -188,20 +156,13 @@ if __name__ == '__main__':
         res = urlopen(req, json.dumps(data))
         data = json.load(res)
         sys.stdout.write('[+] Results will be emailed to: %s\n' % data['email'])
-        sys.stdout.write('[+] Public queue submissions left: %s\n' % data['pubq_limit'])
-        sys.stdout.write('[+] Private queue submissions left: %s\n' % data['privq_limit'])
+        sys.stdout.write('[+] Submissions left: %s\n' % data['privq_limit'])
 
-        if qtype == 'pubq':
-            if (data['pubq_limit'] > 0):
-	        sys.stdout.write('[+] Sending to public queue...\n')
-            else:
-	        sys.stdout.write('[-] ERROR: NO PUBLIC QUEUE SUBMISSIONS LEFT\n')
-                sys.exit(-1)
         else:
             if (data['privq_limit'] > 0):
-	        sys.stdout.write('[+] Sending to private queue...\n')
+	        sys.stdout.write('[+] Sending to the queue...\n')
             else:
-	        sys.stdout.write('[-] ERROR: NO PRIVATE QUEUE SUBMISSIONS LEFT\n')
+	        sys.stdout.write('[-] ERROR: NO QUEUE SUBMISSIONS LEFT. PURCHASE SUBMISSION QUOTA AT https://hashcrack.org/crackq_buy\n')
                 sys.exit(-1)
 
         if _type == 'wpa' or _type == 'ike_md5':
@@ -219,7 +180,7 @@ if __name__ == '__main__':
             _content = base64.b64encode(zlib.compress(_raw))
             f.close()
      
-        data = {'key': API_KEY, 'content': _content, 'type': _type, 'q': qtype}
+        data = {'key': API_KEY, 'content': _content, 'type': _type, 'q': 'privq'}
         req = Request(SERVER + ENDPOINTS['submit'])
         req.add_header('Content-Type', 'application/json')
         res = urlopen(req, json.dumps(data))
